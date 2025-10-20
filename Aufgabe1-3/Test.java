8c71fc75c199dcaa94545003f35c1e2da5640d7d
Tobias Kirnberger <e12424790@student.tuwien.ac.at>
2025-10-20 04:45:32 +0200

    Test: Anpassen der Test Klasse an die letzten Änderungen und hinzufügen der Detail Ausgabe für Simulation 2 und 3

--- Aufgabe1-3/Test.java
+++ Aufgabe1-3/Test.java
@@ -3,7 +3,7 @@
 //Christof - Simulationsklasse und Ausgabe
 
 
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
@@ -12,25 +12,30 @@ public class Test {
         //Liste an Blumen
         //Bienenpopulation
         // Simulation 1:
-        var flowers1 = new TotalFlowerPopulation();
-        var bees1 = new BeePopulation(200);
-        var sim1 = new Simulation(flowers1, bees1);
-        sim1.simulate(1, 25, false );
+        Random beeRandom1 = new Random(67246021);
+        var flowers1 = new TotalFlowerPopulation(21, 1000, 5000, 4.5, 13, 0.3,0.7,394234234);
+        var bees1 = new BeePopulation(800, beeRandom1);
+        var sim1 = new Simulation(flowers1, bees1, 897640);
+        sim1.simulate(10, 25, false );
 
 
         //Simulation 2:
-        var flowers2 = new TotalFlowerPopulation();
-        var bees2 = new BeePopulation(900);
-        var sim2 = new Simulation(flowers2, bees2);
+        Random beeRandom2 = new Random(253721312);
+        var flowers2 = new TotalFlowerPopulation(16, 1000, 6000, 7, 14, 0.28,0.67,54456787);
+        var bees2 = new BeePopulation(2000, beeRandom2); // BeeRandom runs through all runs so every run is different
+        var sim2 = new Simulation(flowers2, bees2, 253721312); //Seed runs through all runs so every run is different
         sim2.simulate(10, 25, false);
 
-        // Simulation 3:
-        var flowers3 = new TotalFlowerPopulation();
-        var bees3 = new BeePopulation(2000);
-        var sim3 = new Simulation(flowers3, bees3);
+//        // Simulation 3:
+        Random beeRandom3 = new Random(4236472);
+        var flowers3 = new TotalFlowerPopulation(24, 1000, 1500, 7, 11, 0.33,0.75,798246);
+        var bees3 = new BeePopulation(1700, beeRandom3);
+        var sim3 = new Simulation(flowers3, bees3, 64782347);
         sim3.simulate(10, 25, false);
 
         System.out.println("DebugInfos (genau Infos)");
         sim1.printDebugInfos();
+        sim2.printDebugInfos();
+        sim3.printDebugInfos();
     }
 }
