c1d345faaddfb0105658b1c7bdc0d7b335ce4562
Tobias Kirnberger <e12424790@student.tuwien.ac.at>
2025-11-10 13:33:45 +0100

    Make Iterator functions default implementations (implementation to be done)

--- Aufgabe4/Bee.java
+++ Aufgabe4/Bee.java
@@ -4,6 +4,10 @@ import java.util.function.Predicate;
 public interface Bee extends Observation, Pollinator, Wasp{
     Observation getPrevious();
     String getTagNumber();
-    Iterator<Bee> sameBee();
-    Iterator<Bee> sameBee(Boolean flip, Predicate<Observation> between);
+    default Iterator<Bee> sameBee(){
+        return null; //TODO: implement me
+    };
+    default Iterator<Bee> sameBee(Boolean flip, Predicate<Observation> between){
+        return null; //TODO: implement me
+    };
 }
