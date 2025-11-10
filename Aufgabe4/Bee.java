85f20b2d3b54f7966f88bac44ac3f8347635f8af
Tobias Kirnberger <e12424790@student.tuwien.ac.at>
2025-11-10 12:42:30 +0100

    Change abstract Class Bee to be an interface instead

--- Aufgabe4/Bee.java
+++ Aufgabe4/Bee.java
@@ -1,29 +1,9 @@
-import java.util.Date;
 import java.util.Iterator;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
-public abstract class Bee implements Observation, Pollinator, Wasp{
-    public Observation earlierObservation;
-    public String tagNumber;
-
-    protected Bee(String comment, Date date) {
-        super(comment, date);
-    }
-    protected Bee(String comment, Date date, Observation earlierObservation) {
-        this.earlierObservation = earlierObservation;
-        super(comment, date);
-    }
-    protected Bee(String comment, Date date, String tagNumber) {
-        this.tagNumber = tagNumber;
-        super(comment, date);
-    }
-    protected Bee(String comment, Date date, Observation earlierObservation, String tagNumber) {
-        this.earlierObservation = earlierObservation;
-        this.tagNumber = tagNumber;
-        super(comment, date);
-    }
-
-    public abstract Iterator<Bee> sameBee();
-    public abstract Iterator<Bee> sameBee(Boolean flip, Predicate<Observation> between);
+public interface Bee extends Observation, Pollinator, Wasp{
+    Observation getEarlierObservation();
+    String getTagNumber();
+    Iterator<Bee> sameBee();
+    Iterator<Bee> sameBee(Boolean flip, Predicate<Observation> between);
 }
