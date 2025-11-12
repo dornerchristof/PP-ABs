2fd7c33000da73bc1b532e8a765e822c5f9461db
Patrick Loser <github.verify097@passmail.com>
2025-11-12 15:06:20 +0100

    WildBee interface

--- Aufgabe4/WildBee.java
+++ Aufgabe4/WildBee.java
@@ -1,2 +1,17 @@
+import java.util.Iterator;
+
 public interface WildBee extends Bee{
+    boolean fromBeekeeping();
+
+    default Iterator<WildBee> wild(boolean fromBeekeeping) {
+        if (this.getIndividuum() != null) return null;
+        return new FilteredObservationIterator<>(this, o ->
+        {
+            if (!(o instanceof WildBee s)) return false;
+            return this.getIndividuum().equals(s.getIndividuum())
+                    && s.fromBeekeeping() == fromBeekeeping;
+        }
+        );
+    }
+
 }
