9f144818518490f074b0ba874b74571c7195654a
cd-z <p_loser_austria@gmx.at>
2025-11-13 16:02:12 +0100

    Update HoneyBee

--- Aufgabe4/HoneyBee.java
+++ Aufgabe4/HoneyBee.java
@@ -1,24 +1,41 @@
 import java.util.Date;
-import java.util.Iterator;
 
 public class HoneyBee implements SocialBee{
     private boolean valid = true;
-    private Date date;
-    private String comment;
+    private final Date date;
+    private final String comment;
+    private Bee earlierObservation = null;
+    private int tagNumber = -1;
+
     protected HoneyBee(String comment, Date date) {
         this.comment = comment;
         this.date = date;
         Test.addObservation(this);
     }
 
+    public HoneyBee(String comment, Date date, int tagNumber) {
+        this(comment, date);
+        this.tagNumber = tagNumber;
+    }
+
+    public HoneyBee(String comment, Date date, Bee earlierObservation) {
+        this(comment, date);
+        this.earlierObservation = earlierObservation;
+    }
+
+    public HoneyBee(String comment, Date date, int tagNumber, Bee earlierObservation) {
+        this(comment, date, tagNumber);
+        this.earlierObservation = earlierObservation;
+    }
+
     @Override
     public Bee getEarlierObservation() {
-        return null;
+        return earlierObservation;
     }
 
     @Override
     public int getTagNumber() {
-        return 0;
+        return tagNumber;
     }
 
     @Override
