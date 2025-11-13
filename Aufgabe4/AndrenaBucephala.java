369f9cb5c988145ce27239571228651c55d190e6
Tobias Kirnberger <e12424790@student.tuwien.ac.at>
2025-11-13 15:30:57 +0100

    Implement and overwrite methods and class varaibles in AndrenaBucephala

--- Aufgabe4/AndrenaBucephala.java
+++ Aufgabe4/AndrenaBucephala.java
@@ -4,18 +4,61 @@ import java.util.function.Predicate;
 
 public class AndrenaBucephala implements CommunalBee{
     private boolean valid = true;
-    protected AndrenaBucephala(String comment, Date date, boolean solitaryLifestyle) {
-
+    private Date date;
+    private String comment;
+    private Bee earlierObservation;
+    private int tagNumber = -1;
+    private Lifestyle lifestyle;
+    public AndrenaBucephala(String comment, Date date) {
+        this.comment = comment;
+        this.date = date;
+        this.lifestyle = Lifestyle.COMMUNAL;
     }
+    public AndrenaBucephala(String comment, Date date, boolean solitaryLifestyle) {
+        this(comment, date);
+        this.lifestyle = solitaryLifestyle ? Lifestyle.SOLITARY : Lifestyle.COMMUNAL;
+    }
+
+    public AndrenaBucephala(String comment, Date date, Bee earlierObservation) {
+        this(comment, date);
+        this.earlierObservation = earlierObservation;
+    }
+
+    public AndrenaBucephala(String comment, Date date, int tagNumber) {
+        this(comment, date);
+        this.tagNumber = tagNumber;
+    }
+
+    public AndrenaBucephala(String comment, Date date, boolean solitaryLifestyle, int tagNumber) {
+        this(comment, date, solitaryLifestyle);
+        this.tagNumber = tagNumber;
+    }
+
+    public AndrenaBucephala(String comment, Date date, boolean solitaryLifestyle, Bee earlierObservation) {
+        this(comment, date, solitaryLifestyle);
+        this.earlierObservation = earlierObservation;
+    }
+
+    public AndrenaBucephala(String comment, Date date, int tagNumber, Bee earlierObservation) {
+        this(comment, date, tagNumber);
+        this.earlierObservation = earlierObservation;
+    }
+
+    public AndrenaBucephala(String comment, Date date, boolean solitaryLifestyle, int tagNumber, Bee earlierObservation) {
+        this(comment, date, solitaryLifestyle, tagNumber);
+        this.earlierObservation = earlierObservation;
+    }
+
+
 
     @Override
     public Date getDate() {
-        return null;
+        return date;
     }
 
     @Override
     public boolean valid() {
-        return false;
+        return valid;
     }
 
     @Override
@@ -25,29 +68,28 @@ public class AndrenaBucephala implements CommunalBee{
 
     @Override
     public Iterator<Observation> earlier() {
-        return null;
+        return new ObservationIterator(this, ObservationIterator.Direction.EARLIER, Test.observations);
     }
 
     @Override
     public Iterator<Observation> later() {
-        return null;
+        return new ObservationIterator(this, ObservationIterator.Direction.LATER, Test.observations);
+    }
+
+    @Override
+    public Bee getEarlierObservation() {
+        return earlierObservation;
+    }
+
+    @Override
+    public int getTagNumber() {
+        return tagNumber;
     }
 
     @Override
     public Lifestyle getLifestyle() {
-        return null;
+        return lifestyle;
     }
-
-    @Override
-    public Iterator<Bee> sameBee() {
-        return null;
-    }
-
-    @Override
-    public Iterator<Bee> sameBee(Boolean flip, Predicate<Observation> between) {
-        return null;
-    }
-
 
     @Override
     public boolean fromBeekeeping() {
