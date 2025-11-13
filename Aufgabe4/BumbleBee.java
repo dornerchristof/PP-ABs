28fbc4212e7b8456fc420accbfdc86f15499fa7a
cd-z <p_loser_austria@gmx.at>
2025-11-13 12:59:02 +0100

    HoneyBee und BumbleBee erstellt

---
+++ Aufgabe4/BumbleBee.java
@@ -0,0 +1,57 @@
+import java.util.Date;
+import java.util.Iterator;
+
+public class BumbleBee implements SocialBee{
+    private boolean valid = true;
+    private Date date;
+    private String comment;
+    protected BumbleBee(String comment, Date date) {
+        this.comment = comment;
+        this.date = date;
+    }
+
+    @Override
+    public Observation getPrevious() {
+        return null;
+    }
+
+    @Override
+    public String getTagNumber() {
+        return "";
+    }
+
+    @Override
+    public Individuum getIndividuum() {
+        return null;
+    }
+
+    @Override
+    public Lifestyle getLifestyle() {
+        return Lifestyle.SOCIAL;
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean valid() {
+        return valid;
+    }
+
+    @Override
+    public void remove() {
+        valid = false;
+    }
+
+    @Override
+    public Iterator<Observation> earlier() {
+        return null;
+    }
+
+    @Override
+    public Iterator<Observation> later() {
+        return null;
+    }
+}
