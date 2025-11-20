f435894f76769077972eb438c1d3773f3c25c16b
Christof Dorner <e52409440@student.tuwien.ac.at>
2025-11-20 11:38:27 +0100

    Aufgabenteilung

--- Aufgabe5/Test.java
+++ Aufgabe5/Test.java
@@ -1,3 +1,7 @@
+
+//Tobias: Modifiable, Ordere, OrdSet, OSet
+//Patrick: Tests, HoneyBee, WildBee, Num
+//Christof: ISet, MSet, OrdSet
 public class Test {
     static void main(String[] args) {
         System.out.println("Aufgabe 5:");
