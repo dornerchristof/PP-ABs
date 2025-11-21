48604ac204e278bee98dcd03760295dfdb54341f
Christof Dorner <e52409440@student.tuwien.ac.at>
2025-11-21 15:17:14 +0100

    Implementierung Wildbee

--- Aufgabe5/WildBee.java
+++ Aufgabe5/WildBee.java
@@ -1,3 +1,29 @@
-public class WildBee extends Bee { //TODO: implments: Modifiable<Integer,WildBee>
+public class WildBee extends Bee implements Modifiable<Integer, WildBee> {
+
+    private final int length;
+    public WildBee(String observationDescription, Integer length) {
+       super(observationDescription) ;
+       this.length = length;
+    }
+
+    public int length(){
+        return this.length;
+    }
+
+    @Override
+    public WildBee add(Integer integer) {
+        if(integer > 0)
+            return new WildBee(this.observationDescription, this.length + integer);
+        else
+            return this;
+    }
+
+    @Override
+    public WildBee subtract(Integer integer) {
+        if(integer > 0 && integer < this.length)
+        return new WildBee(this.observationDescription, this.length - integer);
+        else
+            return this;
+    }
 
 }
