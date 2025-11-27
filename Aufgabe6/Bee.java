05400669bd9794baf8757068ae6febb920f3f5ee
Patrick Loser <github.verify097@passmail.com>
2025-11-27 14:55:23 +0100

    Bee Klasse ausgebaut

--- Aufgabe6/Bee.java
+++ Aufgabe6/Bee.java
@@ -1,6 +1,25 @@
 public abstract class Bee { //TODO: Visibility
-    // TODO: Return Type
-    abstract void collectedFromX();
-    abstract void collectedFromY();
-    abstract void collectedFromZ();
+    protected int collectedX = 0;
+    protected int collectedY = 0;
+    protected int collectedZ = 0;
+    protected int activeFor = 0;
+    protected int totalActiveDays = 0;
+
+    public int collectedFromX() {
+        return collectedX;
+    };
+    public int collectedFromY() {
+        return collectedY;
+    };
+    public int collectedFromZ() {
+        return collectedZ;
+    };
+
+    abstract boolean collect(X plantX, boolean takesAlternative);
+    abstract boolean collect(Y plantY, boolean takesAlternative);
+    abstract boolean collect(Z plantZ, boolean takesAlternative);
+
+    public boolean isActive() {
+        return activeFor < totalActiveDays;
+    };
 }
