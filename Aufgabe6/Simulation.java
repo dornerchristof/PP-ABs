87d2cd1268800fb9be60ff5a2fa49b4e76f4e916
Patrick Loser <github.verify097@passmail.com>
2025-11-27 15:59:29 +0100

    Simulation implementation gestartet

--- Aufgabe6/Simulation.java
+++ Aufgabe6/Simulation.java
@@ -1,3 +1,51 @@
+import java.util.Random;
+
 public class Simulation {
     // TODO: inner implementation of Set (for Object?)
+    private Set<Object> beeSet = new Set<>();
+    private Set<Object> flowerSet = new Set<>();
+    private Random rand = new Random();
+
+    public void simulate() {
+
+        for (int i = 0; i < 7; i++) {
+            addRandomBee();
+            addRandomBee();
+            addRandomFlower();
+            addRandomFlower();
+
+        }
+    }
+    private Bee createRandomBee(int x) {
+        return switch (x) {
+            case 0 -> new U();
+            case 1 -> new V();
+            case 2 -> new W();
+            default -> null;
+        };
+    }
+    private Flower createRandomFlower(int x) {
+        return switch (x) {
+            case 0 -> new X();
+            case 1 -> new Y();
+            case 2 -> new Z();
+            default -> null;
+        };
+    }
+
+    private void addRandomBee() {
+        int x = rand.nextInt(3);
+        int y = rand.nextInt(8) + 2;
+        for (int i = 0; i < y; i++) {
+            beeSet.add(createRandomBee(x ));
+        }
+    }
+    private void addRandomFlower() {
+        int x = rand.nextInt(3);
+        int y = rand.nextInt(8) + 2;
+        for (int i = 0; i < y; i++) {
+            flowerSet.add(createRandomFlower(x ));
+        }
+    }
+
 }
