c364349da7eb20f437eb9aa0c8a9d5ff96cbb901
Patrick Loser <github.verify097@passmail.com>
2025-11-28 15:25:03 +0100

    Simulation hat Statistik Ausgabe

--- Aufgabe6/Simulation.java
+++ Aufgabe6/Simulation.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.concurrent.Flow;
 
 public class Simulation {
     // TODO: inner implementation of Set (for Object?)
@@ -22,7 +23,7 @@ public class Simulation {
                 ((Bee)b).endDay();
             }
         }
-        while (flowerSet.size() > 0 && beeSet.size() > 0) {
+        while (areBeesAndFlowersActive()) {
             simulateBeeDay();
 
             for(Object c : flowerSet){
@@ -32,13 +33,112 @@ public class Simulation {
                 ((Bee)b).endDay();
             }
         }
+        System.out.println("Simulation abgeschlossen");
+        printStats();
+    }
+
+    private void printStats() {
+        Z z = new Z();
+
+        int ZbyV = 0;
+        int YbyV = 0;
+        int XbyV = 0;
+        int ZbyU = 0;
+        int YbyU = 0;
+        int XbyU = 0;
+        int ZbyW = 0;
+        int YbyW = 0;
+        int XbyW = 0;
+
+        for (Object o : beeSet) {
+            Bee bee = (Bee) o;
+            if (bee.prefersFlower(z)){ // Bee is W
+                ZbyW += bee.collectedZ;
+                YbyW += bee.collectedY;
+                XbyW += bee.collectedX;
+            }
+            else if (bee.acceptsFlower(z)){ // Bee is V
+                ZbyV += bee.collectedZ;
+                YbyV += bee.collectedY;
+                XbyV += bee.collectedX;
+            }
+            else{ // Bee is U
+                ZbyU += bee.collectedZ;
+                YbyU += bee.collectedY;
+                XbyU += bee.collectedX;
+            }
+        }
+
+        int WviZ = 0;
+        int WviY = 0;
+        int WviX = 0;
+        int VviZ = 0;
+        int VviY = 0;
+        int VviX = 0;
+        int UviZ = 0;
+        int UviY = 0;
+        int UviX = 0;
+
+        for (Object o : flowerSet) {
+            Flower flower = (Flower) o;
+            if (flower.isPreferredByU()){ // Flower is X
+                WviX += flower.visitedByW;
+                VviX += flower.visitedByV;
+                UviX += flower.visitedByU;
+            }
+            else if (flower.isPreferredByV()){ // Flower is Y
+                WviY += flower.visitedByW;
+                VviY += flower.visitedByV;
+                UviY += flower.visitedByU;
+            }
+            else{ // Flower is Z
+                WviZ += flower.visitedByW;
+                VviZ += flower.visitedByV;
+                UviZ += flower.visitedByU;
+            }
+        }
+        System.out.println("\nSTATISTIK: GESAMMELTER NEKTAR (Bienen-Sicht)");
+        System.out.println("+-------+-------+-------+-------+-------+");
+        System.out.println("| Biene |   X   |   Y   |   Z   | Gesamt|");
+        System.out.println("+-------+-------+-------+-------+-------+");
+        System.out.printf("|   U   | %-5d | %-5d | %-5d | %-5d |\n", XbyU, YbyU, ZbyU, XbyU + YbyU + ZbyU);
+        System.out.printf("|   V   | %-5d | %-5d | %-5d | %-5d |\n", XbyV, YbyV, ZbyV, XbyV + YbyV + ZbyV);
+        System.out.printf("|   W   | %-5d | %-5d | %-5d | %-5d |\n", XbyW, YbyW, ZbyW, XbyW + YbyW + ZbyW);
+        System.out.println("+-------+-------+-------+-------+-------+");
+
+        System.out.println("\nSTATISTIK: ERHALTENE BESUCHE (Blumen-Sicht)");
+        System.out.println("+-------+-------+-------+-------+-------+");
+        System.out.println("| Blume |   U   |   V   |   W   | Gesamt|");
+        System.out.println("+-------+-------+-------+-------+-------+");
+        System.out.printf("|   X   | %-5d | %-5d | %-5d | %-5d |\n", UviX, VviX, WviX, UviX + VviX + WviX);
+        System.out.printf("|   Y   | %-5d | %-5d | %-5d | %-5d |\n", UviY, VviY, WviY, UviY + VviY + WviY);
+        System.out.printf("|   Z   | %-5d | %-5d | %-5d | %-5d |\n", UviZ, VviZ, WviZ, UviZ + VviZ + WviZ);
+        System.out.println("+-------+-------+-------+-------+-------+");
+    }
+
+    private boolean areBeesAndFlowersActive(){
+        boolean flowerAlive = false;
+        boolean beeAlive = false;
+        for(Object c : flowerSet){
+            if(((Flower)c).lives()){
+                flowerAlive = true;
+                break;
+            }
+        }
+        if (!flowerAlive) return false;
+        for(Object b : beeSet){
+            if(((Bee)b).isActive()){
+                beeAlive = true;
+                break;
+            }
+        }
+        return beeAlive;
     }
 
     private void simulateBeeDay() {
         for (Object o : beeSet) {
             Bee bee = (Bee) o;
-            if (!bee.isActive()) {
-                beeSet.remove(beeSet.findByElement(o));
+            if (!bee.isActive() || !acceptableExists(bee)) {
                 continue;
             }
             int randVisits= rand.nextInt(6)+5;
@@ -48,11 +148,7 @@ public class Simulation {
                 while (ftv == null || (favoriteExists && !bee.prefersFlower(ftv)) || (!favoriteExists && !bee.acceptsFlower(ftv))) {
                     ftv = (Flower)flowerSet.findByIndex(rand.nextInt(flowerSet.size()));
                     if (!ftv.lives()){
-                        flowerSet.remove(flowerSet.findByElement(ftv));
                         ftv = null;
-                        if (flowerSet.size() == 0) {
-                            return;
-                        }
                     }
                 }
                 bee.pollinateFlower(ftv);
@@ -62,7 +158,15 @@ public class Simulation {
 
     private boolean favoriteExists(Bee bee) {
         for (Object o : flowerSet) {
-            if (bee.prefersFlower((Flower)o)) {
+            if (bee.prefersFlower((Flower)o) && ((Flower)o).lives()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean acceptableExists(Bee bee) {
+        for (Object o : flowerSet) {
+            if (bee.acceptsFlower((Flower)o) && ((Flower)o).lives()) {
                 return true;
             }
         }
