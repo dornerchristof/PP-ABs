2e462980992bbbc9d9cac3cb683e0a58262334ed
Tobias Kirnberger <e12424790@student.tuwien.ac.at>
2025-11-28 15:32:41 +0100

    Replace Gnerics with Object in Set and Node

--- Aufgabe6/Set.java
+++ Aufgabe6/Set.java
@@ -3,32 +3,32 @@ import java.util.Iterator;
 //TODO: Make this a normal class without Typeparameters
 //TODO: Is this in the right place should it be moved inside the Simulation class? or somewhere else? What about visibility
 @Annotations.Responsible(Annotations.names.Tobias)
-public class Set<T> implements Iterable<T> {
-    private Node<T> head;
+public class Set implements Iterable {
+    private Node head;
     private int size = 0;
 
     public Set(){
         head = null;
     }
 
-    public void add(T node){
-        add(new Node<>(node));
+    public void add(Object node){
+        add(new Node(node));
     }
 
-    public void add(Node<T> node){
+    public void add(Node node){
         if (head == null) head = node;
         else {
-            Node<T> current = head;
+            Node current = head;
             while (current.next != null) current = current.next;
             current.next = node;
         }
         size++;
     }
 
-    public void remove (Node<T> node){
+    public void remove (Node node){
         if (head == node) head = head.next;
         else {
-            Node<T> current = head;
+            Node current = head;
             while (current.next != node) current = current.next;
             current.next = current.next.next;
         }
@@ -39,25 +39,25 @@ public class Set<T> implements Iterable<T> {
         return size;
     }
 
-    public Node<T> findByElement(T element){
-        Node<T> current = head;
+    public Node findByElement(Object element){
+        Node current = head;
         while (current != null && current.elem != element) current = current.next;
         return current;
     }
-    public T findByIndex(int index){
+    public Object findByIndex(int index){
         if (index < 0 || index >= size) return null;
-        Node<T> current = head;
+        Node current = head;
         for (int i = 0; i < index; i++) current = current.next;
         return current.elem;
     }
 
     @Override
-    public Iterator<T> iterator() {
+    public Iterator iterator() {
         return new SetIterator();
     }
 
-    private class SetIterator implements Iterator<T>{
-        private Node<T> current = head;
+    private class SetIterator implements Iterator{
+        private Node current = head;
 
         @Override
         public boolean hasNext() {
@@ -65,8 +65,8 @@ public class Set<T> implements Iterable<T> {
         }
 
         @Override
-        public T next() {
-            T elem = current.elem;
+        public Object next() {
+            Object elem = current.elem;
             current = current.next;
             return elem;
         }
