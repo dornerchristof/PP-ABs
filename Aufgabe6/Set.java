d03e280524759c4b751c469212a479cf0e4bcc8a
Tobias Kirnberger <e12424790@student.tuwien.ac.at>
2025-11-30 15:45:09 +0100

    Set Conditions

--- Aufgabe6/Set.java
+++ Aufgabe6/Set.java
@@ -1,8 +1,7 @@
 import java.util.Iterator;
 
-//TODO: Make this a normal class without Typeparameters
-//TODO: Is this in the right place should it be moved inside the Simulation class? or somewhere else? What about visibility
 @Annotations.Responsible(Annotations.names.Tobias)
+@Annotations.Invariant("size >= 0")
 public class Set implements Iterable {
     private Node head;
     private int size = 0;
@@ -10,29 +9,22 @@ public class Set implements Iterable {
     @Annotations.Responsible(Annotations.names.Tobias)
     private static class Node {
         final Object elem;
-        Node next;
+        private Node next;
 
+        @Annotations.Postcondition("Node wurde erstellt")
         public Node(Object elem) {
             this.elem = elem;
         }
 
-        public Object getElem() {
-            return elem;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
     }
 
+    @Annotations.Postcondition("neues Set wurde erstellt")
+    @Annotations.Postcondition("Set ist leer")
     public Set(){
         head = null;
     }
-
+    @Annotations.Postcondition("obj wurde am Ende des Sets eingefügt")
+    @Annotations.Postcondition("size++")
     public boolean add(Object obj){
         Node node = new Node(obj);;
         if (head == null) head = node;
@@ -45,6 +37,9 @@ public class Set implements Iterable {
         return true;
     }
 
+    @Annotations.Precondition("node ist in Set enthalten")
+    @Annotations.Postcondition("node wurde aus Set entfernt")
+    @Annotations.Postcondition("size--")
     public void remove (Object node){
         if (head.elem == node) head = head.next;
         else {
@@ -54,11 +49,13 @@ public class Set implements Iterable {
         }
         size--;
     }
-
+    @Annotations.Postcondition("Gibt die menge der Elmente im Set zurück size >= 0")
     public int size(){
         return size;
     }
 
+    @Annotations.Postcondition("Falls der Index innerhalb des Arrys liegt wird das Objekt in der zugehörigen Node zurückgegeben")
+    @Annotations.Postcondition("Falls sich der Index außerhalb des Arrays befindet wird null zurückgegeben")
     public Object findByIndex(int index){
         if (index < 0 || index >= size) return null;
         Node current = head;
@@ -66,19 +63,23 @@ public class Set implements Iterable {
         return current.elem;
     }
 
+    @Annotations.Postcondition("Ein Iterator über das Set wurde zurückgegeben")
     @Override
     public Iterator iterator() {
         return new SetIterator();
     }
 
+    @Annotations.Responsible(Annotations.names.Christof)
+    @Annotations.ClientHistoryConstraint("next() darf nur ausgeführt werden wenn hasNext == true")
     private class SetIterator implements Iterator{
         private Node current = head;
 
+        @Annotations.Postcondition("Gibt true zurück falls es ein nächstes Element gibt sonst false")
         @Override
         public boolean hasNext() {
             return current != null;
         }
-
+        @Annotations.Postcondition("Gibt das nächste Element zurückk")
         @Override
         public Object next() {
             Object elem = current.elem;
